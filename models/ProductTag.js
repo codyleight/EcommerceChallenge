a43c82a344908d56fafa57737299fd076620ec64
Cody Thompson <codylthomp@gmail.com>
2023-11-21 10:59:47 -0600

    Post Put and Delete added for categories and product

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,21 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
-  },
+    /* id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    tag_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },*/
+  }, 
   {
     sequelize,
     timestamps: false,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,15 +32,45 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
- 
+  try {
+    const locationData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
